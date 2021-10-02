d7ccd501003061760ffdb5e8098b25f6f781482e
plaban <plabanmondal02@gmail.com>
2021-10-02 11:37:59 +0530

    feat: add ascending order and descending order

--- python/bubbleSort.py
+++ python/bubbleSort.py
@@ -1,13 +1,24 @@
-def bubbleSort(list):
-    for i in range(len(list)-1, 0, -1):
+# Ascending Order
+def bubbleSortAscending(list):
+    for i in range(len(list) - 1, 0, -1):
         for j in range(i):
-            if list[j] > list[j+1]:
+            if list[j] > list[j + 1]:
                 temp_element = list[j]
-                list[j] = list[j+1]
-                list[j+1] = temp_element
+                list[j] = list[j + 1]
+                list[j + 1] = temp_element
 
 
-num_list = [3,9,3,5,2,6,8,7,1]
+# Descending Order
+def bubbleSortDescending(list):
+    for i in range(len(list) - 1, 0, -1):
+        for j in range(i):
+            if list[j] < list[j + 1]:
+                temp_element = list[j]
+                list[j] = list[j + 1]
+                list[j + 1] = temp_element
 
-bubbleSort(num_list)
+
+num_list = [3, 9, 3, 5, 2, 6, 8, 7, 1]
+
+bubbleSortDescending(num_list)
 print(num_list)
